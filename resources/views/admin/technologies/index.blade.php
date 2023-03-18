1719b33e05c8ce4ca1531f870b1194b36a960bcf
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-18 15:17:04 +0100

    creating types index

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-
     <table class="table table-hover mt-5">
         <thead>
-            <th class="text-primary">Technologies</th>
-            <th class="text-end text-primary">Edit</th>
+            <th class="col-2 text-primary">Technologies</th>
+            <th class="col-1 text-primary text-center">Icon</th>
+            <th class="col-2 text-primary">CSS Class</th>
+            <th class="col-7 text-primary text-end">Edit</th>
         </thead>
         <tbody>
             @forelse($technologies as $technology)
             <tr>
                 <td>{{ $technology->label }}</td>
+                <td class="text-center"><i class="text-primary fa-solid {{$technology->class_icon}}"></i></td>
+                <td>{{$technology->class_icon}}</td>
                 <td class="text-end"><a href="{{route('admin.technologies.edit', $technology->id)}}" class="btn btn-outline-primary btn-sm"><i class="fa-solid fa-pencil"></i></a></td>
             </tr>
             @empty
@@ -24,7 +26,5 @@
     
     <!-- route to create a new tehcnology -->
     <a href="{{route('admin.technologies.create')}}" class="btn btn-outline-primary">Create a New One</a>
-
-</div>
 
 @endsection
